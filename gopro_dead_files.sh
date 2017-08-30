0fb957cc83f26791791f2f09ef927db0f67427d5
ldriv <aspectplatinum@gmail.com>
2017-08-30 11:19:35 -0700

    Create gopro_dead_files.sh
    Shell script to remove extra hidden gopro files after deleting gopro videos w/out app

---
+++ gopro_dead_files.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# Shell script to remove extra hidden gopro files after deleting gopro videos w/out app
+# 2017 08.30.2017 LDRIV https://www.linkedin.com/in/ldriv/
+
+# Get list of files in current dir
+filenames=`ls|tr '.' ' '|awk '{print $1}'`
+
+# Get base filename counts and only print ones that have 2 occurrences
+# Gopro stores 3 files total for each video, MP4 THM LRV
+basenames=`echo "$filesnames"|sort -n -k2,2|uniq -c|sort -rk 1|grep "2 "|awk '{print $2}'`
+
+# Delete files
+for file in ${basenames[@]}; do 
+  rm $x.LRV
+  rm $x.THM 
+done
